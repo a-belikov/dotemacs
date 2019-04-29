cdf1f3dc8177224a74850d1d84b0b5edb3fe5514
Alexey Belikov <alexey@it-pr.ru>
2019-04-29 11:03:59 +0300

    SPC as leader

--- init.el
+++ init.el
@@ -5,28 +5,8 @@
 ;; Save cutomization to file
 (setq custom-file "~/.emacs.d/custom.el")
 
-
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-'("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-(package-refresh-contents)
-(package-install 'use-package))
-
-;;Load configuration files
-;; (load-file "~/.emacs.d/ab/00-system.el")
-;; (load-file "~/.emacs.d/ab/01-repositories.el")
-
-;; (load-file "~/.emacs.d/ab/02-packages.el")
-;; (load-file "~/.emacs.d/ab/03-settings.el")
-;; (load-file "~/.emacs.d/ab/04-keybindings.el")
-;; 
-(org-babel-load-file (expand-file-name "~/.emacs.d/ab/00-sets.el.org"))
-
+;; load
+(load-file "~/.emacs.d/vimode-emacs.init.el")
 ;; Config with secret key
 (load-file "~/.emacs.d/ab/11-private.el")
 
