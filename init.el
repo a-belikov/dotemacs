c5647ce3ba907e352a1830a39e258b6835b6d785
Alexey Belikov <alexey@it-pr.ru>
2019-10-04 15:09:02 +0300

    init GGTAGS

--- init.el
+++ init.el
@@ -20,21 +20,8 @@
 (eval-when-compile
   (require 'use-package))
 
-
-;; (add-to-list 'package-archives
-;; '("melpa" . "https://melpa.org/packages/"))
-;; (package-initialize)
-
-;;Load configuration files
-;; (load-file "~/.emacs.d/ab/00-system.el")
-;; (load-file "~/.emacs.d/ab/01-repositories.el")
-
-;; (load-file "~/.emacs.d/ab/02-packages.el")
-;; (load-file "~/.emacs.d/ab/03-settings.el")
-;; (load-file "~/.emacs.d/ab/04-keybindings.el")
-;; 
 (org-babel-load-file (expand-file-name "~/.emacs.d/ab/00-sets.el.org"))
-
+;; (load-file "~/.emacs.d/ab/00-sets.el.el")
 
 
 ;; ligatures
